84b56cd8d74f94c921d1d40ff992da60148f4068
Mdeca <mdeca.dev@gmail.com>
2018-03-23 03:40:17 +0100

    Added mdeca_install.sh

---
+++ mdeca_install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Simple script to setup dev environment
+#
+## 
+
+
+echo "Mdeca dev script"
+read -p "Press enter to continue."
+
+
+# add repository's :
+sudo add-apt-repository ppa:jonathonf/backports -y
+sudo add-apt-repository ppa:jonathonf/python-3.6 -y
+
+# Update and upgrade:
+sudo apt-get update
+sudo apt-get -y upgrade
+
+# Install python3.6:
+sudo apt-get install -y python3.6
+
+# Install git-core:
+sudo apt-get install -y git-core
+
+# Import dotfiles from github:
+alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"
+dotfiles config --local status.showUntrackedFiles no
+git clone --bare https://github.com/Mdeca/dotfiles.git $HOME/.dotfiles.git
+dotfiles checkout
+
+# Install zsh and ohmyzsh:
+sudo apt-get install -y zsh
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+chsh -s `which zsh`
+
+read -p "Done !! Press enter to reboot"
+reboot
