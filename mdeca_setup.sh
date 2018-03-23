6cf380e1e9d29edb9ea57418232d5c84efc38f03
name <email>
2018-03-23 17:46:06 +0100

    update

--- mdeca_setup.sh
+++ mdeca_setup.sh
@@ -1,23 +1,48 @@
-# Mdece_dotfiles.sh
+#!/bin/bash
+# Simple script to setup dev environment
 #
-##
+## 
 
 
-# Import dotfiles from github:
+echo "MdecaSetup.sh"
+read -p "Press enter to continue."
 
-echo "Setting dotfiles alias"
-alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"
 
-echo "Cloning the repository"
-git clone --bare https://github.com/Mdeca/dotfiles.git $HOME/.dotfiles.git
-alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"
+# add repository's :
+echo "Adding Repositorys"
+sudo add-apt-repository ppa:jonathonf/backports -y
+sudo add-apt-repository ppa:jonathonf/python-3.6 -y
 
-echo "Checkout from master"
-dotfiles checkout -f
+# Update and upgrade:
+echo "Update and Upgrade"
+sudo apt-get update
+sudo apt-get -y upgrade
 
-echo "Source the new .zshrc"
-source $HOME/.zshrc
+# Install python3.6:
+echo "Installing Python 3.6"
+sudo apt-get install -y python3.6
 
-echo "Show repository status"
-dotfiles status
+# Install git-core:
+echo "Installing Git-core"
+sudo apt-get install -y git-core
 
+# Install zsh and ohmyzsh:
+echo "Installing zsh and setting up ohmyzsh theme"
+sudo apt-get install -y zsh
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+
+# Cleaning:
+echo "Cleaning Up !!"
+sudo apt-get autoremove
+sudo apt-get -y autoclean
+sudo apt-get -y clean
+
+# Change default shell:
+echo "Changing default shell"
+chsh -s $(which zsh)
+
+read -p "Done !! Press enter to reboot"
+echo "Rebooting..."
+reboot
+
+#END#
