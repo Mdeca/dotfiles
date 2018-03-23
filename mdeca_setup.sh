091f87215a641d2d318c85e263d02af3cc454262
Mdeca <mdeca.dev@gmail.com>
2018-03-23 16:53:28 +0100

    Updated mdeca_dotfiles.sh and mdeca_setup.sh

---
+++ mdeca_setup.sh
@@ -0,0 +1,23 @@
+# Mdece_dotfiles.sh
+#
+##
+
+
+# Import dotfiles from github:
+
+echo "Setting dotfiles alias"
+alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"
+
+echo "Cloning the repository"
+git clone --bare https://github.com/Mdeca/dotfiles.git $HOME/.dotfiles.git
+alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"
+
+echo "Checkout from master"
+dotfiles checkout -f
+
+echo "Source the new .zshrc"
+source $HOME/.zshrc
+
+echo "Show repository status"
+dotfiles status
+
